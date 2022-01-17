794583dc158dbd779fb265b9a243cd4f7e295066
Vivek Shakya <shakya.vivek20@gmail.com>
2022-01-17 20:38:36 +0545

    Handle commas and spaces

--- make-m3u.bat
+++ make-m3u.bat
@@ -2,13 +2,19 @@
 
 SETLOCAL
 
-SET directoryName=%*
+SET "directoryName=%~1"
+SET tempVar=%2
 SET /A numberOfFiles=0
+
+@REM Handles commas in the main-directory name
+if defined tempVar (
+    SET directoryName=%*
+)
 
 CALL :GetPlaylistName "%directoryName%" playlistName
 echo # > "%playlistName%"
 
-FOR /R "%directoryName%" %%i IN (*.mp4 *.mpv) DO (
+FOR /R "%directoryName%" %%i IN (*.mp4 *.mkv) DO (
     set /a numberOfFiles+=1
     echo %%i >> "%playlistName%"
 )
